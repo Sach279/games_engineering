920337a2adf04a9b88d936a136704fdea9ff3df8
Sachdeva <40444896@live.napier.ac.uk>
2024-09-27 17:31:33 +0100

    beninning of space invaders

---
+++ 2_invaders/main.cpp
@@ -0,0 +1,55 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+sf::Texture spritesheet;
+sf::Sprite invader;
+
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+
+void Load() {
+    if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+        cerr << "Failed to load spritesheet!" << std::endl;
+    }
+    invader.setTexture(spritesheet);
+    invader.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(32, 32)));
+}
+
+void Render(RenderWindow& window) {
+    window.draw(invader);
+}
+
+void Update(RenderWindow& window) {
+    // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+    // check and consume events
+    Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+            return;
+        }
+    }
+
+    // Quit Via ESC Key
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+        window.close();
+    }
+}
+
+int main() {
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+    Load();
+    while (window.isOpen()) {
+        window.clear();
+        Update(window);
+        Render(window);
+        window.display();
+    }
+    return 0;
+}
